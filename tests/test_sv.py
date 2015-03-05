047e570dec9ff264fffa3f404611c2851291d8f1
Michael Conlon <mconlon@ufl.edu>
2015-03-04 20:00:07 -0500

    test_sv.py for Simple VIVO test cases

---
+++ tests/test_sv.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env/python
+# coding=utf-8
+""" test_sv.py -- Test cases for Simple VIVO
+"""
+
+__author__ = "Michael Conlon"
+__copyright__ = "Copyright 2015, University of Florida"
+__license__ = "BSD 3-Clause license"
+__version__ = "1.00"
+
+import unittest
+from vivopump import new_uri, read_csv, vivo_query, write_update_def, repair_email, repair_phone_number, comma_space, \
+    improve_title, make_update_query, read_update_def, make_rdf_term, get_graph, \
+    improve_dollar_amount, InvalidDataException, improve_date, improve_deptid, improve_sponsor_award_id
+from pump import Pump
+
+
+class ParamTestCase(unittest.TestCase):
+    def test_parameters(self):
+        uri = new_uri()
+        self.assertTrue(len(uri) > 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
