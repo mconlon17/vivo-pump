79f5229260caca436680738ee1f3ffcf036cb292
Michael Conlon <mconlon@ufl.edu>
2015-07-13 15:56:31 -0400

    parsing author names uf_examples/publications/author_prep_filter.py

--- uf_examples/publications/author_prep_filter.py
+++ uf_examples/publications/author_prep_filter.py
@@ -33,13 +33,54 @@ __version__ = "0.01"
 from vivopump import read_csv_fp, write_csv_fp, key_string
 import sys
 
+
+def parse_author_data(author_data, affiliation_data, max_list_length=50):
+    """
+    Parse the author string from TR bibtex.  It has the names of each author.  For each author, determine from
+    the affiliation string if the author is the corresponding author (true or false) and if they are a UF author
+    (true or false).  Return six data elements for each author -- display_name, last, first, middle names and
+    the two true/false values.  Return a list of authors.  Each author has the six elements
+    :param author_data:
+    :param affiliation_data:
+    :param max_list_length: Author list maximum length.  To prevent Physics papers from swamping the process
+    :return: author_list.  A list of authors.  Each author is a dict with six elements.
+    """
+    author_list = []
+    author_names = author_data.split(' and ')
+    list_length = 0
+    for display_name in author_names:
+        list_length += 1
+        if list_length > max_list_length:
+            break
+        author_dict = {'display_name': display_name}
+        if ',' in display_name:
+            k = display_name.find(',')
+            author_dict['last'] = display_name[0:k]
+            remainder = display_name[k + 2:]
+            if ' ' in remainder:
+                k = remainder.find(' ')
+                author_dict['first'] = remainder[0:k]
+            else:
+                author_dict['first'] = remainder
+                author_dict['middle'] = ''
+        else:
+            author_dict['last'] = display_name
+            author_dict['first'] = ''
+            author_dict['middle'] = ''
+        author_list.append(author_dict)
+    print >>sys.stderr, author_list
+    return author_list
+
 data_in = read_csv_fp(sys.stdin)
 var_names = data_in[data_in.keys()[1]].keys()  # create a list of var_names from the first row
 print >>sys.stderr, "Columns in", var_names
 data_out = {}
+row_out = 0
 keep_names = set(['remove', 'uri', 'title', 'display_name', 'first', 'last', 'middle', 'corresponding', 'uf'])
 for row, data in data_in.items():
     new_data =dict(data)
+
+    author_data = parse_author_data(new_data['author'], new_data['affiliation'])
 
     # Add these columns
 
@@ -59,7 +100,11 @@ for row, data in data_in.items():
         if name not in keep_names:
             del new_data[name]
 
-    data_out[row] = new_data
+    for author in author_data:
+        row_out += 1
+        data_out[row_out] = dict(new_data)
+        for key in author.keys():
+            data_out[row_out][key] = author[key]
 var_names = data_out[data_out.keys()[1]].keys()  # create a list of var_names from the first row
 print >>sys.stderr, "Columns out", var_names
 write_csv_fp(sys.stdout, data_out)
