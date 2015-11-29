15122e3a7da82b39088d7f9c0bd59805e90b269e
Michael Conlon <mconlon@ufl.edu>
2015-11-29 18:04:31 -0500

    draft get_person_vivo_pmids, get_person_catalyst_ids

--- pubmed.py
+++ pubmed.py
@@ -31,7 +31,48 @@ HOST = "profiles.catalyst.harvard.edu"
 API_URL = "/services/GETPMIDs/default.asp"
 
 
-def get_pmids(first, middle, last, email, affiliation=None, yes=None, no=None):
+def get_person_catalyst_pmids(uri, query_parms):
+    """
+    Given a person uri, collect the attributes needed to call get_pmids and return two lists:
+    a list of pubs for the person found in VIVO, and a list of pubs for the person found by
+    the catalyst service
+    :param uri: the uri of a person in VIVO
+    :return: A dictionary of two lists, the vivo_pmids and the catalyst_pmids
+    """
+    from vivopump import vivo_query
+    query = """
+    SELECT ?first ?middle ?last ?email ?affiliation
+    WHERE {
+      <{}>
+    }
+    """
+    query = query.format(uri)
+    a = vivo_query(query, query_parms)
+    first = a['results']['bindings'][0]['first']['value']
+    middle = None
+    last = None
+    emails = None
+    affiliations = None
+    return get_catalyst_pmids(first, middle, last, emails, affiliations)
+
+
+def get_person_vivo_pmids(uri, query_parms):
+    """
+    Given the uri of a person, query VIVO to a list of the person's publications and their pmids
+    :param uri:
+    :return: a dictionary keyed by pmid with uris of the pubs for each pmid
+    """
+    from vivopump import vivo_query
+    query = "select ?pmid (MAX(?puri) AS ?puri) where { <{}> relates ?a. ?a a vivo:Authorship . ?a relates ?puri . " \
+            "?puri a bibo:AcademicArticle . ?puri vivo:pmid ?pmid .} GROUP BY ?pmid"
+    query = query.format(uri)
+    a = vivo_query(query, query_parms)
+    pmid = [x['pmid']['value'] for x in a['results']['bindings']]
+    puri = [x['puri']['value'] for x in a['results']['bindings']]
+    return dict(zip(pmid, puri))
+
+
+def get_catalyst_pmids(first, middle, last, email, affiliation=None, yes=None, no=None):
     """
     Given an author's identifiers and affiliation information, optional lists of pmids, call the catalyst service
     to retrieve PMIDS for the author and return a list of PMIDS
