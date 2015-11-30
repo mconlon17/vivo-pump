47676bacd75bc714c51cbef85494912327acbddc
Andrei Sura <sura.andrei@gmail.com>
2015-11-30 11:42:44 -0500

    Cleanup uf_examples/publications/publisher_columns_filter.py

--- uf_examples/publications/filters/publisher_columns_filter.py
+++ uf_examples/publications/filters/publisher_columns_filter.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
-    manage_columns_filter.py -- add needed columns, remove unused columns
+publisher_columns_filter.py -- add needed columns, remove unused columns
 """
 
 __author__ = "Michael Conlon"
@@ -13,32 +14,30 @@ from vivopump import read_csv_fp, write_csv_fp, improve_org_name
 import sys
 
 data_in = read_csv_fp(sys.stdin)
-var_names = data_in[data_in.keys()[1]].keys()  # create a list of var_names from the first row
+# create a list of var_names from the first row
+var_names = data_in[data_in.keys()[1]].keys()
 print >>sys.stderr, "Columns in", var_names
+
 data_out = {}
 keep_names = set(['remove', 'uri', 'name', 'type'])
+
 for row, data in data_in.items():
-    new_data =dict(data)
+    new_data = dict(data)
 
     # Add these columns
-
     new_data['remove'] = ''
     new_data['uri'] = ''
     new_data['name'] = improve_org_name(new_data['publisher']).upper()
     new_data['type'] = 'publisher'
 
     # Delete everything not in the keep_names set
-
     for name in new_data.keys():
         if name not in keep_names:
             del new_data[name]
 
     data_out[row] = new_data
-var_names = data_out[data_out.keys()[1]].keys()  # create a list of var_names from the first row
+
+# create a list of var_names from the first row
+var_names = data_out[data_out.keys()[1]].keys()
 print >>sys.stderr, "Columns out", var_names
 write_csv_fp(sys.stdout, data_out)
-
-
-
-
-
