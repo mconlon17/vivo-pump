7866ef822045d7a5cececbfffd3f85473fe75b81
Andrei Sura <sura.andrei@gmail.com>
2015-12-01 17:29:46 -0500

    Fix the code in `filters/pub_match_filter.py` to use `doi` instead of `issn` for matching

--- uf_examples/publications/filters/pub_match_filter.py
+++ uf_examples/publications/filters/pub_match_filter.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 """
-    pub_match_filter.py -- find the pubs in VIVO, and match them by title to the pubs
-    in the source.
+pub_match_filter.py -- find the academic articles in VIVO, and match them by
+    the doi to the pubs in the source.
 
-    There are two inputs:
-    1. pubs in VIVO.  Keyed by title
-    2. pubs in the source.  Keyed by title
+There are two inputs:
+    1. pubs in VIVO keyed by doi
+    2. pubs in the source keyed by doi
 
-    There are three cases
-    1. pub in VIVO and in source => add to update data with uri
-    1. pub in VIVO, not in source => nothing to do
-    1. pub not in VIVO, is in source => Add to update data with blank uri (to be assigned during update)
-
-    See CHANGELOG.md for history
+There are three cases
+    - pub in VIVO and in source => add to update data with uri
+    - pub in VIVO, not in source => nothing to do
+    - pub not in VIVO, is in source => Add to update data with blank uri
+    (to be assigned during update)
 """
 
 __author__ = "Michael Conlon"
@@ -21,28 +21,29 @@ __copyright__ = "Copyright 2015 (c) Michael Conlon"
 __license__ = "New BSD License"
 __version__ = "0.01"
 
-from vivopump import read_csv_fp, write_csv_fp, get_vivo_pubs, get_parms
+from vivopump import read_csv_fp, write_csv_fp, get_parms
+from vivopump import get_vivo_academic_articles
 import sys
 
 parms = get_parms()
 data_in = read_csv_fp(sys.stdin)
 print >>sys.stderr, len(data_in)
+
 data_out = {}
-vivo_pubs = get_vivo_pubs(parms)  # get dictionary of pub uri keyed by simplified name
+# get dictionary of pub uri keyed by doi
+vivo_pubs = get_vivo_academic_articles(parms)
+
 print >>sys.stderr, 'VIVO pubs', len(vivo_pubs)
 print >>sys.stderr, vivo_pubs
 
 for row, data in data_in.items():
     data_out[row] = data
-    if data['issn'] not in vivo_pubs:  # name is not vivo.  These are the ones to add
+
+    # name is not vivo.  These are the ones to add
+    if data['doi'] not in vivo_pubs:
         data_out[row]['uri'] = ''
     else:
-        data_out[row]['uri'] = vivo_pubs[data['issn']]
+        data_out[row]['uri'] = vivo_pubs[data['doi']]
 
 print >>sys.stderr, 'data out', len(data_out)
 write_csv_fp(sys.stdout, data_out)
-
-
-
-
-
